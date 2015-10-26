e9daa063ced98a4f5ae263a2a062099b39925203
Richard Domander <rdomander@rvc.ac.uk>
2015-10-26 14:58:59 +0000

    Starting point for refactoring (doube.jama.Matrix replaced with Jama).

---
+++ BoneJProtosMVN/src/main/java/common/MatrixUtil.java
@@ -0,0 +1,24 @@
+package common;
+
+import Jama.Matrix;
+
+/**
+ * Created by Richard Domander on 26/10/15.
+ */
+public class MatrixUtil {
+    /**
+     *  Returns the diagonal of the given matrix as a new column vector
+     */
+    public static Matrix diagonalAsColumn(Matrix m)
+    {
+        int rows = Math.min(m.getColumnDimension(), m.getRowDimension());
+        Matrix column = new Matrix(rows, 1);
+
+        for (int i=0; i<rows; i++) {
+            column.set(i, 0, m.get(i, i));
+        }
+
+        return column;
+    }
+
+}
