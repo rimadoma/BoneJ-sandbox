4f9539753730c4c7907284e403f36d3b8cef0dbf
Richard Domander <rdomander@rvc.ac.uk>
2015-10-28 15:59:55 +0000

    Added settings dialog

--- BoneJProtosMVN/src/main/java/common/Common.java
+++ BoneJProtosMVN/src/main/java/common/Common.java
@@ -8,6 +8,11 @@ public class Common {
     public static final int BINARY_BLACK = 0x00;
     public static final int BINARY_WHITE = 0xFF;
 
+    public static final String ANISOTROPY_WARNING = "This image contains anisotropic voxels, which will\n"
+            + "result in incorrect thickness calculation.\n\n"
+            + "Consider rescaling your data so that voxels are isotropic\n"
+            + "(Image > Scale...).\n\n" + "Continue anyway?";
+
     public static double clamp(double value, double min, double max) {
         if (Double.compare(value, min) < 0) {
             return min;
--- BoneJProtosMVN/src/main/java/protoPlugins/Thickness.java
+++ BoneJProtosMVN/src/main/java/protoPlugins/Thickness.java
@@ -1,13 +1,20 @@
 package protoPlugins;
 
+import common.Common;
+import common.ImageCheck;
 import ij.IJ;
 import ij.ImagePlus;
+import ij.gui.GenericDialog;
 import net.imagej.ImageJ;
-
 import org.scijava.command.Command;
 import org.scijava.log.LogService;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
+import org.scijava.ui.UIService;
+
+//import LocalThickness_.LocalThickness;
+
+import static org.scijava.ui.DialogPrompt.*;
 
 /**
  * @author <a href="mailto:rdomander@rvc.ac.uk">Richard Domander</a>
@@ -17,12 +24,52 @@ import org.scijava.plugin.Plugin;
 public class Thickness implements Command
 {
     // The following service parameters are populated automatically
-    // by the service framework before the command is executed.
+    // by the SciJava service framework before this command plugin is executed.
     @Parameter
     private LogService log;
 
+    @Parameter
+    private UIService ui;
+
+    // @todo Read from a file?
+    private static final String HELP_URL = "http://bonej.org/thickness";
+
     private ImagePlus image = null;
 
+    private GenericDialog setupDialog;
+
+    boolean doThickness = true;
+    boolean doSpacing = false;
+    boolean doGraphic = true;
+    boolean doRoi = false;
+    boolean doMask = true;
+
+    /**
+     * @todo Remove legacy code, and replace with proper ImageJ2 to show setup dialog
+     */
+    private void createSetupDialog()
+    {
+        setupDialog = new GenericDialog("Plugin options");
+        setupDialog.addCheckbox("Thickness", doThickness);
+        setupDialog.addCheckbox("Spacing", doSpacing);
+        setupDialog.addCheckbox("Graphic Result", doGraphic);
+        setupDialog.addCheckbox("Use_ROI_Manager", doRoi);
+        setupDialog.addCheckbox("Mask thickness map", doMask);
+        setupDialog.addHelp(HELP_URL);
+    }
+
+    private void getProcessingSettingsFromDialog()
+    {
+        doThickness = setupDialog.getNextBoolean();
+        doSpacing = setupDialog.getNextBoolean();
+        doGraphic = setupDialog.getNextBoolean();
+        doRoi = setupDialog.getNextBoolean();
+        doMask = setupDialog.getNextBoolean();
+    }
+
+    /**
+     * @return true if the current image open in ImageJ can be processed by this plugin
+     */
     private boolean setCurrentImage()
     {
         try {
@@ -32,17 +79,43 @@ public class Thickness implements Command
             return false;
         }
 
-        return true;
+        if (!ImageCheck.isBinary(image)) {
+            log.error("8-bit binary (black and white only) image required.");
+            return false;
+        }
+
+        final double ANISOTROPY_TOLERANCE = 1E-3;
+        if (ImageCheck.isVoxelIsotropic(image, ANISOTROPY_TOLERANCE)) {
+            return true;
+        }
+
+        Result result = ui.showDialog(Common.ANISOTROPY_WARNING, "Anisotropic voxels", MessageType.WARNING_MESSAGE,
+                OptionType.OK_CANCEL_OPTION);
+
+        return result != Result.CANCEL_OPTION;
     }
 
     @Override
-    public void run() {
-        if (setCurrentImage()) {
-            log.info("Opened an image successfully");
+    public void run()
+    {
+        if (!ImageCheck.isBoneJEnvironmentValid()) {
+            return;
         }
+
+        if (!setCurrentImage()) {
+            return;
+        }
+
+        createSetupDialog();
+        setupDialog.showDialog();
+        if (setupDialog.wasCanceled()) {
+            return;
+        }
+        getProcessingSettingsFromDialog();
     }
 
-    public static void main(final String... args) {
+    public static void main(final String... args)
+    {
         final ImageJ ij = net.imagej.Main.launch(args);
         IJ.open();
         ij.command().run(Thickness.class, true);
