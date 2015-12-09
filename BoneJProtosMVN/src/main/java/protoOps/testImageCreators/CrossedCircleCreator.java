77cc862d58deaa45aba84fde02435bd9f84be9cf
Richard Domander <rdomander@rvc.ac.uk>
2015-12-09 13:05:04 +0000

    Fix CrossedCircleCreator display bug
    Fixed an issue where the user wouldn't see anything in the image produced by the Op

--- BoneJProtosMVN/src/main/java/protoOps/testImageCreators/CrossedCircleCreator.java
+++ BoneJProtosMVN/src/main/java/protoOps/testImageCreators/CrossedCircleCreator.java
@@ -1,21 +1,13 @@
 package protoOps.testImageCreators;
 
 import ij.ImagePlus;
-import ij.process.ByteProcessor;
-import ij.process.ImageProcessor;
-import net.imagej.Dataset;
 import net.imagej.DatasetService;
 import net.imagej.ImageJ;
-import net.imagej.ImgPlus;
 import net.imagej.ops.Op;
 import net.imagej.ops.OpEnvironment;
-import net.imglib2.img.ImagePlusAdapter;
-import net.imglib2.type.numeric.integer.UnsignedByteType;
 import org.scijava.ItemIO;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
-
-import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * An Op which creates an image of a circle and a cross.
@@ -30,10 +22,13 @@ public class CrossedCircleCreator implements Op
     private DatasetService datasetService;
 
     @Parameter(min = "10", required = false)
-    private int imageSize = 100;
+    private int imageSize = 200;
 
+    /**
+     * @todo    Find out why the user sees nothing if this ImagePlus is converted into a Dataset (pixel values are ok).
+     */
     @Parameter(type = ItemIO.OUTPUT)
-    private Dataset testImage = null;
+    private ImagePlus testImage = null;
 
     @Override
     public void run() {
@@ -52,17 +47,9 @@ public class CrossedCircleCreator implements Op
 
     /**
      * Draw a circle with a vertical and horizontal line crossing it to this.testImage
-     *
-     * @throws  NullPointerException if this.datasetService == null
-     * @return  A Dataset containing a white crossed circle
-     *
      */
     private void createCrossedCircle() {
-        checkNotNull(datasetService, "No dataset service found");
-
-        ImagePlus imagePlus = StaticTestImageHelper.createCrossedCircle(imageSize);
-        ImgPlus<UnsignedByteType> imgPlus = ImagePlusAdapter.wrapImgPlus(imagePlus);
-        testImage = datasetService.create(imgPlus);
+        testImage = StaticTestImageHelper.createCrossedCircle(imageSize);
     }
 
     public static void main(final String... args) throws Exception {
