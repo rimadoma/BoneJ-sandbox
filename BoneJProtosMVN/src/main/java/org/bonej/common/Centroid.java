715db12a0465a7e5ad76ec7346c81f0f9d2a64bf
Richard Domander <rdomander@rvc.ac.uk>
2015-12-11 11:41:33 +0000

    Polish Centroid
    Add special cases and tests for getCentroid

--- BoneJProtosMVN/src/main/java/org/bonej/common/Centroid.java
+++ BoneJProtosMVN/src/main/java/org/bonej/common/Centroid.java
@@ -4,13 +4,34 @@ import sc.fiji.analyzeSkeleton.Point;
 
 import java.util.ArrayList;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 /**
- * @author Michael Doube
- * @author <a href="mailto:rdomander@rvc.ac.uk">Richard Domander</a>
+ * @author  Michael Doube
+ * @author  Richard Domander
  */
 public class Centroid
 {
+    //Indices of the coordinates in the Centroid array
+    public final static int X_INDEX = 0;
+    public final static int Y_INDEX = 1;
+    public final static int Z_INDEX = 2;
+
+    /**
+     * Calculates the geometric center of all the given points
+     *
+     * @param   points  A list of points in 3D
+     * @throws  NullPointerException if points == null
+     * @return  An array of {x,y,z} coordinates of the centroid point
+     *          Returns {NaN, NaN, NaN} if points is empty
+     */
     public static double[] getCentroid(ArrayList<Point> points) {
+        checkNotNull(points, "List of points is null");
+
+        if (points.isEmpty()) {
+            return new double[]{Double.NaN, Double.NaN, Double.NaN};
+        }
+
         double xSum = 0;
         double ySum = 0;
         double zSum = 0;
---
+++ BoneJProtosMVN/src/test/java/org/bonej/common/CentroidTest.java
@@ -0,0 +1,63 @@
+package org.bonej.common;
+
+import org.junit.Test;
+import sc.fiji.analyzeSkeleton.Point;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit tests for the org.bonej.common.Centroid class
+ *
+ * @author Richard Domander
+ */
+public class CentroidTest {
+    private final static double DELTA = 1E-12;
+
+    @Test (expected = NullPointerException.class)
+    public void testGetCentroidThrowsNullPointerExceptionIfPointListIsNull() throws Exception {
+        Centroid.getCentroid(null);
+    }
+
+    @Test
+    public void testGetCentroidReturnsNaNArrayIfPointListIsEmpty() throws Exception {
+        double emptyListResult[] = Centroid.getCentroid(new ArrayList<>());
+
+        assertEquals("Coordinate array has wrong dimensions", 3, emptyListResult.length);
+        for (double coordinate : emptyListResult) {
+            assertTrue("Empty list should return NaN coordinates", Double.isNaN(coordinate));
+        }
+    }
+
+    @Test
+    public void testGetCentroidOnePoint() throws Exception {
+        Point point = new Point(3, 4, 5);
+        ArrayList<Point> points = new ArrayList<>();
+        points.add(point);
+
+        double result[] = Centroid.getCentroid(points);
+
+        assertPointCoordinatesEqual(point, result);
+    }
+
+    @Test
+    public void testCentroid() throws Exception {
+        ArrayList<Point> tiltedSquare = new ArrayList<>();
+        tiltedSquare.add(new Point(0, 0, 0));
+        tiltedSquare.add(new Point(2, 0, 0));
+        tiltedSquare.add(new Point(2, 2, 2));
+        tiltedSquare.add(new Point(0, 2, 2));
+        Point expectedCentroid = new Point(1, 1, 1);
+
+        double result[] = Centroid.getCentroid(tiltedSquare);
+
+        assertPointCoordinatesEqual(expectedCentroid, result);
+    }
+
+    private void assertPointCoordinatesEqual(Point expected, double[] coordinates) {
+        assertEquals("Unexpected x-coordinate", expected.x, coordinates[Centroid.X_INDEX], DELTA);
+        assertEquals("Unexpected y-coordinate", expected.y, coordinates[Centroid.Y_INDEX], DELTA);
+        assertEquals("Unexpected z-coordinate", expected.z, coordinates[Centroid.Z_INDEX], DELTA);
+    }
+}
