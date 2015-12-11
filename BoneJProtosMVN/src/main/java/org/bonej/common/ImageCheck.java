b1d7db326f14f2ebf0dc901683e2213e97fe4b91
Richard Domander <rdomander@rvc.ac.uk>
2015-12-11 10:40:50 +0000

    Clean unused classes & methods

--- BoneJProtosMVN/src/main/java/org/bonej/common/ImageCheck.java
+++ BoneJProtosMVN/src/main/java/org/bonej/common/ImageCheck.java
@@ -2,7 +2,6 @@ package org.bonej.common;
 
 import ij.IJ;
 import ij.ImagePlus;
-import ij.ImageStack;
 import ij.measure.Calibration;
 import ij.process.ImageStatistics;
 
@@ -52,17 +51,6 @@ public class ImageCheck {
     }
 
     /**
-     * Check if an image is a multi-slice image stack
-     *
-     * @param   imp image to test
-     * @pre     imp != null
-     * @return  true if the image has >= 2 slices
-     */
-    public static boolean isMultiSlice(ImagePlus imp) {
-        return imp.getStackSize() >= 2;
-    }
-
-    /**
      * Check if the image's voxels are isotropic in all 3 dimensions (i.e. are
      * placed on a cubic grid)
      *
@@ -93,106 +81,6 @@ public class ImageCheck {
             return false;
 
         return true;
-    }
-
-    /**
-     * Run isVoxelIsotropic() with a default tolerance of 0%
-     *
-     * @param   imp   image to test
-     * @return false if voxel dimensions are not equal
-     */
-    public static boolean isVoxelIsotropic(ImagePlus imp) {
-        return isVoxelIsotropic(imp, 0.0);
-    }
-
-    /**
-     * Check that the voxel thickness is correct
-     *
-     * @param imp
-     * @return voxel thickness based on DICOM header information. Returns -1 if
-     *         there is no DICOM slice position information.
-     */
-    public double dicomVoxelDepth(ImagePlus imp) {
-        Calibration cal = imp.getCalibration();
-        double vD = cal.pixelDepth;
-
-        String position = getDicomAttribute(imp, 1, "0020,0032");
-        if (position == null) {
-            IJ.log("No DICOM slice position data");
-            return -1;
-        }
-        String[] xyz = position.split("\\\\");
-        double first = 0;
-        if (xyz.length == 3) // we have 3 values
-            first = Double.parseDouble(xyz[2]);
-        else
-            return -1;
-
-        position = getDicomAttribute(imp, imp.getStackSize(), "0020,0032");
-        xyz = position.split("\\\\");
-        double last = 0;
-        if (xyz.length == 3) // we have 3 values
-            last = Double.parseDouble(xyz[2]);
-        else
-            return -1;
-
-        double sliceSpacing = Math.abs((last - first)
-                / (imp.getStackSize() - 1));
-
-        String units = cal.getUnits();
-
-        double error = Math.abs((sliceSpacing - vD) / sliceSpacing) * 100;
-
-        if (vD != sliceSpacing) {
-            IJ.log(imp.getTitle() + ":\n" + "Current voxel depth disagrees by "
-                    + error + "% with DICOM header slice spacing.\n"
-                    + "Current voxel depth: " + IJ.d2s(vD, 6) + " " + units
-                    + "\n" + "DICOM slice spacing: " + IJ.d2s(sliceSpacing, 6)
-                    + " " + units + "\n" + "Updating image properties...");
-            cal.pixelDepth = sliceSpacing;
-            imp.setCalibration(cal);
-        } else
-            IJ.log(imp.getTitle() + ": Voxel depth agrees with DICOM header.\n");
-        return sliceSpacing;
-    }
-
-    /**
-     * Get the value associated with a DICOM tag from an ImagePlus header
-     *
-     * @param imp
-     * @param slice
-     * @param tag
-     *            , in 0000,0000 format.
-     * @return the value associated with the tag
-     */
-    private String getDicomAttribute(ImagePlus imp, int slice, String tag) {
-        ImageStack stack = imp.getImageStack();
-        String header = stack.getSliceLabel(slice);
-        // tag must be in format 0000,0000
-        if (slice < 1 || slice > stack.getSize()) {
-            return null;
-        }
-        if (header == null) {
-            return null;
-        }
-        String attribute = " ";
-        String value = " ";
-        int idx1 = header.indexOf(tag);
-        int idx2 = header.indexOf(":", idx1);
-        int idx3 = header.indexOf("\n", idx2);
-        if (idx1 >= 0 && idx2 >= 0 && idx3 >= 0) {
-            try {
-                attribute = header.substring(idx1 + 9, idx2);
-                attribute = attribute.trim();
-                value = header.substring(idx2 + 1, idx3);
-                value = value.trim();
-                // IJ.log("tag = " + tag + ", attribute = " + attribute
-                // + ", value = " + value);
-            } catch (Throwable e) {
-                return " ";
-            }
-        }
-        return value;
     }
 
     /**
@@ -246,67 +134,6 @@ public class ImageCheck {
         }
 
         return isIJVersionValid();
-    }
-
-    /**
-     * Check that IJ has enough memory to do the job
-     *
-     * @param memoryRequirement
-     *            Estimated required memory
-     * @return True if there is enough memory or if the user wants to continue.
-     *         False if the user wants to continue despite a risk of
-     *         insufficient memory
-     */
-    public static boolean checkMemory(long memoryRequirement) {
-        if (memoryRequirement > IJ.maxMemory()) {
-            String message = "You might not have enough memory to run this job.\n"
-                    + "Do you want to continue?";
-            if (IJ.showMessageWithCancel("Memory Warning", message)) {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return true;
-        }
-    }
-
-    public static boolean checkMemory(ImagePlus imp, double ratio) {
-        double size = ((double) imp.getWidth() * imp.getHeight() * imp
-                .getStackSize());
-        switch (imp.getType()) {
-            case ImagePlus.GRAY8:
-            case ImagePlus.COLOR_256:
-                break;
-            case ImagePlus.GRAY16:
-                size *= 2.0;
-                break;
-            case ImagePlus.GRAY32:
-                size *= 4.0;
-                break;
-            case ImagePlus.COLOR_RGB:
-                size *= 4.0;
-                break;
-        }
-        long memoryRequirement = (long) (size * ratio);
-        return checkMemory(memoryRequirement);
-    }
-
-    /**
-     * Guess whether an image is Hounsfield unit calibrated
-     *
-     * @param imp
-     * @return true if the image might be HU calibrated
-     */
-    public static boolean huCalibrated(ImagePlus imp) {
-        Calibration cal = imp.getCalibration();
-        double[] coeff = cal.getCoefficients();
-        if (!cal.calibrated() || cal == null
-                || (cal.getCValue(0) == 0 && coeff[1] == 1)
-                || (cal.getCValue(0) == Short.MIN_VALUE && coeff[1] == 1)) {
-            return false;
-        } else
-            return true;
     }
 
     /**
--- BoneJProtosMVN/src/test/java/org/bonej/geometry/VectorsTest.java
+++ BoneJProtosMVN/src/test/java/org/bonej/geometry/VectorsTest.java
@@ -6,58 +6,11 @@ import org.junit.Test;
 import javax.vecmath.Vector3d;
 
 /**
- *  A class to test that javax.vecmath.Vector3d and bonej.geometry.Vectors
- *  calculate similar results. The rationale for these tests is to
- *  ensure that the results BoneJ returns won't change numerically
- *  (precision, rounding errors) etc.
+ * Unit tests for BoneJ Vectors class
  *
- * @author <a href="mailto:rdomander@rvc.ac.uk">Richard Domander</a>
+ * @author Richard Domander
  */
 public class VectorsTest {
-    double x1 = 5;
-    double y1 = 3;
-    double z1 = 1;
-
-    double x2 = 8;
-    double y2 = 6;
-    double z2 = 4;
-
-    Vector3d u = new Vector3d(x1, y1, z1);
-    Vector3d v = new Vector3d(x2, y2, z2);
-
-    final double EPSILON = 1e-12;
-
-    @Test
-    public void testCrossProduct()
-    {
-        double[] expected = { 6, -12, 6 };
-
-        double[] result = Vectors.crossProduct(x1, y1, z1, x2, y2, z2);
-
-        assertEquals(result.length, expected.length);
-        assertArrayEquals(expected, result, EPSILON);
-
-        Vector3d w = new Vector3d();
-        double vector3dResult [] = new double[3];
-
-        w.cross(u, v);
-        w.get(vector3dResult);
-
-        assertArrayEquals(expected, vector3dResult, EPSILON);
-    }
-
-    @Test
-    public void testNorm()
-    {
-        Vector3d w = new Vector3d();
-        w.normalize(v);
-
-        Vector3d expected = new Vector3d(v);
-        expected.normalize();
-
-        assertEquals(expected.epsilonEquals(w, EPSILON), true);
-    }
-
     @Test
     public void testJoinedVectorAngle() throws Exception {
         final Vector3d originalU = new Vector3d(1.0, 2.0, 3.0);
