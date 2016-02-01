e7b1a0f4a0a2ff081ebfb4c6f1e26efaa0fca2bb
Richard Domander <rdomander@rvc.ac.uk>
2016-02-01 09:38:02 +0000

    Polish Dataset example code

--- BoneJProtosMVN/src/main/java/protoOps/datasetExample/DatasetWrappingExample.java
+++ BoneJProtosMVN/src/main/java/protoOps/datasetExample/DatasetWrappingExample.java
@@ -1,10 +1,15 @@
 package protoOps.datasetExample;
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.base.Preconditions.checkNotNull;
+/*
+ * To the extent possible under law, the ImageJ developers have waived
+ * all copyright and related or neighboring rights to this tutorial code.
+ *
+ * See the CC0 1.0 Universal license for details:
+ *     http://creativecommons.org/publicdomain/zero/1.0/
+ */
 
-import java.util.stream.IntStream;
-
+import ij.ImagePlus;
+import ij.ImageStack;
 import net.imagej.Dataset;
 import net.imagej.DatasetService;
 import net.imagej.ImageJ;
@@ -16,34 +21,32 @@ import net.imagej.ops.Op;
 import net.imagej.ops.OpEnvironment;
 import net.imglib2.img.ImagePlusAdapter;
 import net.imglib2.type.numeric.integer.UnsignedByteType;
-
-import org.bonej.common.ImageCheck;
 import org.scijava.AbstractContextual;
 import org.scijava.ItemIO;
 import org.scijava.convert.ConvertService;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 
-import ij.ImagePlus;
-import ij.ImageStack;
+import java.util.stream.IntStream;
 
 /**
- * An example class to test wrapping and unwrapping of a ImageJ2 Dataset
+ * An example class to test wrapping and unwrapping of a ImageJ2 Dataset. An Op
+ * which inverts the colors of the given image.
  *
- * How to run via the Java API:
- * 1) Create an instance of the class
- * 2) Call setContext with e.g. ImageJ#getContext()
- * 3) Call setDataset
- * 4) Call run
- * 5) Call getDataset to get the result
+ * Internally the plugin operates with IJ1 style ImagePlus, but externally it
+ * operates with IJ2 Datasets
  *
- * How to run via an opService e.g. ImageJ.op()
- * 1) Create an instance of ImageJ
- * 2) Call final Object image = ij.op().run("datasetExample", dataset)
- * 3) Object image contains the resulting dataset
+ * How to run via the Java API: 1) Create an instance of the class 2) Call
+ * setContext with e.g. ImageJ#getContext() 3) Call setDataset 4) Call run 5)
+ * Call getDataset to get the result
  *
- * The class AbstractContextual is inherited so that you can run this class via Java API.
- * When you run it via an opService the convertService field is populated automatically.
+ * How to run via an opService e.g. ImageJ.op() 1) Create an instance of ImageJ
+ * 2) Call final Object outputDataset = ij.op().run("datasetExample",
+ * inputDataset) 3) Object outputDataset contains the resulting dataset
+ *
+ * The class AbstractContextual is inherited so that you can run this class via
+ * Java API. When you run it via an opService the service fields are populated
+ * automatically.
  *
  * @author Richard Domander
  */
@@ -85,7 +88,8 @@ public class DatasetWrappingExample extends AbstractContextual implements Op {
         checkNotNull(datasetService, "Missing services - did you remember to call setContext?");
 
         if (image == null) {
-            // the plugin is (probably) run from an opService, check the dataset obtained as @Parameter
+            // the plugin is (probably) run from an opService, check the dataset
+            // obtained as @Parameter
             setDataset(dataset);
         }
 
@@ -95,53 +99,85 @@ public class DatasetWrappingExample extends AbstractContextual implements Op {
         dataset = datasetService.create(imgPlus);
     }
 
-    //region -- Utility methods --
+    // region -- Utility methods --
+
+    /** Run the plugin via Java API */
     public static void main(final String... args) throws Exception {
         int width = 320;
         int height = 200;
         int depth = 10;
+
+        // Create an instance of ImageJ
         final ImageJ ij = new ImageJ();
 
-        final Dataset inputDataset = createMonochromeDataset(ij ,width, height, depth, 0xFF);
+        // NB if you show the UI after setting the Dataset, an the program
+        // automatically shows the image
+        // This side effect has been reported
+        ij.ui().showUI();
+
+        // Create a dataset for the Plugin
+        final Dataset inputDataset = createMonochromeDataset(ij, width, height, depth, 0xFF);
 
         final DatasetWrappingExample datasetExample = new DatasetWrappingExample();
+
+        // Set the context for the plugin to populate service @Parameters
         datasetExample.setContext(ij.getContext());
 
         try {
             datasetExample.setDataset(inputDataset);
             datasetExample.run();
-        } catch (Exception e) {
+        } catch (NullPointerException | IllegalArgumentException e) {
             System.out.println("Exception: " + e.getMessage());
-            return;
         }
 
-        ij.ui().showUI();
+        // Display the image the plugin produced
+        Dataset outputDataset = datasetExample.getDataset();
+        ij.ui().show(outputDataset);
     }
 
     public static Dataset createMonochromeDataset(final ImageJ ijInstance, final int width, final int height,
-                                                  int depth, int color) {
+                                                  final int depth, final int color) {
         final long[] dims = {width, height, depth};
         final AxisType[] axisTypes = {Axes.X, Axes.Y, Axes.Z};
         Dataset dataset = ijInstance.dataset().create(new UnsignedByteType(), dims, "Test image", axisTypes);
 
         IntStream.range(0, depth).forEach(i -> {
             final byte[] data = new byte[width * height];
-            IntStream.range(0, data.length).forEach(j -> data[j] = (byte)color);
+            IntStream.range(0, data.length).forEach(j -> data[j] = (byte) color);
             dataset.setPlane(i, data);
         });
 
         return dataset;
     }
-    //endregion
+    // endregion
 
-    //region -- Helper methods --
-    private void checkDataset(Dataset dataset) {
+    // region -- Helper methods --
+    private void checkNotNull(final Object object, final String message) {
+        if (object == null) {
+            throw new NullPointerException(message);
+        }
+    }
+
+    private void checkArgument(final boolean argument, final String message) {
+        if (!argument) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    /**
+     * Check whether the Op can operate with the given dataset
+     *
+     * @throws NullPointerException     if dataset == null
+     * @throws IllegalArgumentException if the dataset has wrong number of dimensions
+     * @throws IllegalArgumentException if the dataset cannot be converted into an ImagePlus
+     */
+    private void checkDataset(final Dataset dataset) {
         checkNotNull(dataset, "Dataset cannot be null");
         checkDatasetDimensions(dataset);
         checkArgument(convertService.supports(dataset, ImagePlus.class), "Cannot convert given dataset");
     }
 
-    private void checkDatasetDimensions(Dataset dataset) {
+    private void checkDatasetDimensions(final Dataset dataset) {
         checkArgument(dataset.numDimensions() == 3, "The plugin is meant only for 3D images");
         CalibratedAxis axes[] = new CalibratedAxis[3];
         dataset.axes(axes);
@@ -150,11 +186,9 @@ public class DatasetWrappingExample extends AbstractContextual implements Op {
         }
     }
 
-    private void checkImage(ImagePlus imagePlus) {
-        checkArgument(imagePlus.getBitDepth() == 8, "Image must be 8-bit");
-        checkArgument(ImageCheck.isBinary(imagePlus), "Image must be binary");
-    }
-
+    /**
+     * Inverts the value of each bit in each pixel
+     */
     private void makeNegative() {
         final int depth = image.getNSlices();
         final ImageStack stack = image.getStack();
@@ -168,10 +202,7 @@ public class DatasetWrappingExample extends AbstractContextual implements Op {
     }
 
     private void setImagePlus() {
-        //@todo Find out why the call to convert pops up a weird image window
-        ImagePlus imagePlus = convertService.convert(dataset, ImagePlus.class);
-        checkImage(imagePlus);
-        image = imagePlus;
+        image = convertService.convert(dataset, ImagePlus.class);
     }
-    //endregion
+    // endregion
 }
