fc91421422fe9cad9884580f533b0d86bf2cb5f4
Richard Domander <rdomander@rvc.ac.uk>
2016-02-04 15:01:14 +0000

    Fix test
    Fixed VolumeFractionVoxelTest.testVolumeFractionWithMask with mocking

--- BoneJProtosMVN/src/main/java/protoOps/volumeFraction/VolumeFractionVoxelTest.java
+++ BoneJProtosMVN/src/main/java/protoOps/volumeFraction/VolumeFractionVoxelTest.java
@@ -1,6 +1,7 @@
 package protoOps.volumeFraction;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -16,8 +17,11 @@ import protoOps.testImageCreators.StaticTestImageHelper;
 import ij.ImagePlus;
 import ij.ImageStack;
 import ij.gui.Roi;
+import ij.measure.Calibration;
 import ij.plugin.frame.RoiManager;
+import ij.process.ByteProcessor;
 import ij.process.ImageProcessor;
+import ij.process.ImageStatistics;
 
 /**
  * Unit tests for the VolumeFractionVoxel class
@@ -91,37 +95,72 @@ public class VolumeFractionVoxelTest {
 
     /**
      * Test that volumes and volume fraction are correctly calculated in the area defined by a mask
-     * @todo Test fails because after setup the mask in cuboid == null
      */
     @Test
     public void testVolumeFractionWithMask() throws Exception {
-        final ImagePlus cuboid = StaticTestImageHelper.createCuboid(WIDTH, HEIGHT, DEPTH, WHITE, 0);
-        final double CUBOID_VOLUME = WIDTH * HEIGHT * DEPTH * 0.75;
-        final double TOTAL_VOLUME = CUBOID_VOLUME;
+        final double FOREGROUND_VOLUME = WIDTH * HEIGHT * DEPTH * 0.75;
+        final double TOTAL_VOLUME = FOREGROUND_VOLUME;
+        int white = 0xFF;
 
-        // Add an L-shaped mask on each slice in the stack of the cuboid
-        Polygon polygon = new Polygon();
-        polygon.addPoint(0, 0);
-        polygon.addPoint(WIDTH, 0);
-        polygon.addPoint(WIDTH, HEIGHT / 2);
-        polygon.addPoint(WIDTH / 2, HEIGHT / 2);
-        polygon.addPoint(WIDTH / 2, HEIGHT);
-        polygon.addPoint(0, HEIGHT);
-        polygon.addPoint(0, 0);
+        ImageProcessor mask = createLMask(WIDTH, HEIGHT);
 
-        final ImageStack stack = cuboid.getStack();
-        for (int z = 1; z <= stack.getSize(); z++) {
-            final ImageProcessor processor = stack.getProcessor(z);
-            processor.setRoi(polygon);
-        }
+        // Mock Image* classes to make getMask() work
+        // In a normal instance of ImageProcessor getMask() returns null,
+        // even right after setMask().
+        ImageProcessor mockProcessor = mock(ImageProcessor.class);
+        when(mockProcessor.get(anyInt(), anyInt())).thenReturn(white);
+        when(mockProcessor.getRoi()).thenReturn(new Rectangle(0, 0, WIDTH, HEIGHT));
+        when(mockProcessor.getMask()).thenReturn(mask);
 
-        // run op
-        final ArrayList<Double> volumes = (ArrayList<Double>) imageJInstance.op().run("volumeFractionVoxel", cuboid,
+        ImageStack mockStack = mock(ImageStack.class);
+        when(mockStack.getWidth()).thenReturn(WIDTH);
+        when(mockStack.getHeight()).thenReturn(HEIGHT);
+        when(mockStack.getSize()).thenReturn(DEPTH);
+        when(mockStack.getProcessor(anyInt())).thenReturn(mockProcessor);
+
+        ImageStatistics statistics = new ImageStatistics();
+        statistics.histogram = new int[256];
+
+        ImagePlus mockCuboid = mock(ImagePlus.class);
+        when(mockCuboid.getStack()).thenReturn(mockStack);
+        when(mockCuboid.getBitDepth()).thenReturn(8);
+        when(mockCuboid.getStatistics()).thenReturn(statistics);
+        when(mockCuboid.getType()).thenReturn(ImagePlus.GRAY8);
+        when(mockCuboid.getCalibration()).thenReturn(new Calibration());
+
+        final ArrayList<Double> volumes = (ArrayList<Double>) imageJInstance.op().run("volumeFractionVoxel", mockCuboid,
                 MIN_THRESHOLD, MAX_THRESHOLD);
 
-        assertEquals("Sample foreground volume is incorrect", CUBOID_VOLUME, volumes.get(FG_INDEX), DELTA);
+        assertEquals("Sample foreground volume is incorrect", FOREGROUND_VOLUME, volumes.get(FG_INDEX), DELTA);
         assertEquals("Total sample volume is incorrect", TOTAL_VOLUME, volumes.get(TOTAL_INDEX), DELTA);
-        assertEquals("Volume ratio is incorrect", CUBOID_VOLUME / TOTAL_VOLUME, volumes.get(RATIO_INDEX),
+        assertEquals("Volume ratio is incorrect", FOREGROUND_VOLUME / TOTAL_VOLUME, volumes.get(RATIO_INDEX),
                 DELTA);
+    }
+
+    /**
+     * Creates an L-shaped mask that blocks the lower right-hand corner of an image
+     *
+     * NB width & height need to be the same than the dimensions of the image this mask is used on.
+     *
+     * @param width     Width of the mask
+     * @param height    Height of the mask
+     * @return An ImageProcessor that can be passed to ImageProcessor#setMask
+     */
+    private ImageProcessor createLMask(final int width, final int height) {
+        ImageProcessor mask = new ByteProcessor(width, height);
+        ImageProcessor tmp = new ByteProcessor(width, height);
+
+        Polygon polygon = new Polygon();
+        polygon.addPoint(0, 0);
+        polygon.addPoint(width, 0);
+        polygon.addPoint(width, height / 2);
+        polygon.addPoint(width / 2, height / 2);
+        polygon.addPoint(width / 2, height);
+        polygon.addPoint(0, height);
+        polygon.addPoint(0, 0);
+        tmp.setRoi(polygon);
+
+        mask.setPixels(tmp.getMask().getPixels());
+        return mask;
     }
 }
